37ad0cc675c1d2a5d3c5a1bc4783b281e107fcf9
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-11-16 19:40:02 +0530

    establishing backend to portfolio

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening to ${port}...`))
+
+const dbURi = process.env.DATABASE;
+mongoose.connect(dbURi)
+  .then(() => console.log(`Connected to database...`))
+  .catch(err => console.log(`Error while connecting to database: ${err}`))
+
+const {Projects} = require("./schema");
+
+app.get("/", (req, res) => {
+  res.send("redirect to /xyz :)");
+})
+
+app.post("/input-projects", (req, res) => {
+  const data = new Projects(req.body);
+  res.send(data);
+  data.save()
+    .then(() => console.log(`Project saved successfully`))
+    .catch(err => console.log(`Error while saving project: ${err}`))
+})
+
+app.get("/projects", (req, res) => {
+  Projects.find()
+    .then(response => res.send(response))
+    .catch(err => console.log(`Error while retrieving projects: ${err}`))
+})
---
+++ Backend/schema.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema({
+  name: { type: String },
+  description: { type: String },
+  github: { type: String },
+  website: { type: String },
+  image: { type: String },
+});
+
+const Projects = mongoose.model("Project", ProjectSchema);
+
+module.exports = { Projects };
