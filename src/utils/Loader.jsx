0b9852846dfd8108f6b112b663696ef791394b9b
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-11-29 23:53:50 +0530

    adding loader

--- src/utils/Loader.jsx
+++ src/utils/Loader.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
-import loading from "../Images/loading.png";
+import loading from "../Images/loading.json";
 import "../App.css";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 const Loader = () => {
   return (
     <div className="loader">
       {/* <img src={loading} alt="loading" /> */}
-      <lottie-player
-        src="https://assets5.lottiefiles.com/datafiles/zc3XRzudyWE36ZBJr7PIkkqq0PFIrIBgp4ojqShI/newAnimation.json"
-        background="transparent"
-        speed="1"
-        style="width: 300px; height: 300px;"
-        loop
-        controls
+      <Player
         autoplay
-      ></lottie-player>
+        loop
+        src={loading}
+        style={{ height: "300px", width: "300px" }}
+      ></Player>
     </div>
   );
 }
